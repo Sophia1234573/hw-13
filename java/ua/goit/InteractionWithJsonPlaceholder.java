ba2a0464c133d3f423a7bf78bcce28f4035ca6fc
sophia <sophiadenisovichgovtva@gmail.com>
2021-03-26 01:16:47 +0200

    Added classes. Changed return types in methods.

--- java/ua/goit/InteractionWithJsonPlaceholder.java
+++ java/ua/goit/InteractionWithJsonPlaceholder.java
@@ -1,6 +1,7 @@
 package ua.goit;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
 import java.net.URI;
@@ -8,6 +9,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
+import java.util.List;
 
 public class InteractionWithJsonPlaceholder {
     private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
@@ -17,24 +19,25 @@ public class InteractionWithJsonPlaceholder {
     private static final String USERS_URL_BY_USERNAME = "https://jsonplaceholder.typicode.com/users?username=";
     private static final String USERS_URL_POSTS = "https://jsonplaceholder.typicode.com/posts/";
 
-    public String createNewUser(String path) throws IOException, InterruptedException {
+    public User createNewUser(User user) throws IOException, InterruptedException {
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(USERS_URL))
                 .header("Content-type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofFile(Path.of(path)))
+                .POST(HttpRequest.BodyPublishers.ofString(GSON.toJson(user)))
                 .build();
         HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
+        return GSON.fromJson(response.body(), User.class);
     }
 
-    public String updateUser(int id, String path) throws IOException, InterruptedException {
+    public User updateUser(int id, User user) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(String.format("%s%d", USERS_URL_BY_ID, id)))
                 .header("Content-type", "application/json")
-                .PUT(HttpRequest.BodyPublishers.ofFile(Path.of(path)))
+                .PUT(HttpRequest.BodyPublishers.ofString(GSON.toJson(user)))
                 .build();
         HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
+        return GSON.fromJson(response.body(), User.class);
     }
 
     public int deleteUserById (int id) throws IOException, InterruptedException{
@@ -47,37 +50,39 @@ public class InteractionWithJsonPlaceholder {
         return response.statusCode();
     }
 
-    public String getUsers() throws IOException, InterruptedException {
+    public List<User> getUsers() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(USERS_URL))
                 .header("Content-type", "application/json")
                 .GET()
                 .build();
         HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-        return GSON.toJson(response.body());
+        List<User> users = GSON.fromJson(response.body(), new TypeToken<List<User>>(){}.getType());
+        return users;
     }
 
-    public String getUserById(int id) throws IOException, InterruptedException{
+    public User getUserById(int id) throws IOException, InterruptedException{
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(String.format("%s%d", USERS_URL_BY_ID, id)))
                 .header("Content-type", "application/json")
                 .GET()
                 .build();
         HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-        return GSON.toJson(response.body());
+        return GSON.fromJson(response.body(), User.class);
     }
 
-    public String getUserByUsername(String username) throws IOException, InterruptedException{
+    public List<User> getUserByUsername(String username) throws IOException, InterruptedException{
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(String.format("%s%s", USERS_URL_BY_USERNAME, username)))
                 .header("Content-type", "application/json")
                 .GET()
                 .build();
         HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-        return GSON.toJson(response.body());
+        List<User> users = GSON.fromJson(response.body(), new TypeToken<List<User>>(){}.getType());
+        return users;
     }
 
-    public String getComments(int x) throws IOException, InterruptedException {
+    public List<Comment> getComments(int x) throws IOException, InterruptedException {
         int y = x * 10;
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(String.format("%s%d%s", USERS_URL_POSTS, y, "/comments")))
@@ -88,16 +93,18 @@ public class InteractionWithJsonPlaceholder {
         HttpResponse<Path> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers
                 .ofFile(Path.of("D:\\Sophia\\Java\\GoIT\\module13\\HTTP-hw\\src\\main\\resources\\user-" + x + "-post-" + y + "-comments.json")));
         HttpResponse<String> responseToPrint = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-        return GSON.toJson(responseToPrint.body());
+        List<Comment> comments = GSON.fromJson(responseToPrint.body(), new TypeToken<List<Comment>>(){}.getType());
+        return comments;
     }
 
-    public String getTodosForUser(int id) throws IOException, InterruptedException {
+    public List<Todo> getTodosForUser(int id) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(String.format("%s%d%s", USERS_URL_BY_ID, id, "/todos?completed=false")))
                 .header("Content-type", "application/json")
                 .GET()
                 .build();
         HttpResponse<String> response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
+        List<Todo> todos = GSON.fromJson(response.body(), new TypeToken<List<Todo>>(){}.getType());
+        return todos;
     }
 }
--- java/ua/goit/Main.java
+++ java/ua/goit/Main.java
@@ -3,19 +3,17 @@ package ua.goit;
 import java.io.IOException;
 
 public class Main {
-    private static final String PATH_TO_NEW_USER = "D:\\Sophia\\Java\\GoIT\\module13\\HTTP-hw\\src\\main\\resources\\userToPost.json";
-    private static final String PATH_TO_UPDATE_USER = "D:\\Sophia\\Java\\GoIT\\module13\\HTTP-hw\\src\\main\\resources\\userToUpdate.json";
 
     public static void main(String[] args) throws IOException, InterruptedException {
         InteractionWithJsonPlaceholder interaction = new InteractionWithJsonPlaceholder();
         //Create new user
         System.out.println("Created user:");
-        System.out.println(interaction.createNewUser(PATH_TO_NEW_USER));
+        System.out.println(interaction.createNewUser(createDefaultUser()));
 
         //Update user
         System.out.println("-----------------------");
         System.out.println("Updated user:");
-        System.out.println(interaction.updateUser(5, PATH_TO_UPDATE_USER));
+        System.out.println(interaction.updateUser(5, createDefaultUser()));
 
         //Delete user by id
         System.out.println("-----------------------");
@@ -34,7 +32,7 @@ public class Main {
         //Information about user by username
         System.out.println("-----------------------");
         System.out.println("Information about user by username:");
-        System.out.println(interaction.getUserByUsername("Karianne"));
+        System.out.println(interaction.getUserByUsername("Leopoldo_Corkery"));
 
         //Get comments of last users post
         System.out.println("-----------------------");
@@ -45,5 +43,31 @@ public class Main {
         System.out.println("-----------------------");
         System.out.println("Todos:");
         System.out.println(interaction.getTodosForUser(1));
+    }
+
+    private static User createDefaultUser() {
+        User newUser = new User.Builder()
+                .id(1)
+                .name("Sophia")
+                .username("sophia1234573")
+                .email("sophiadenisovichgovtva@gmail.com")
+                .website("goit.ua")
+                .phone("0669008172")
+                .company(new Company.Builder()
+                        .name("Romaguera-Crona")
+                        .bs("harness real-time e-markets")
+                        .catchPhrase("Multi-layered client-server neural-net").build())
+                .address(new Address.Builder()
+                        .zipcode("92998-3874")
+                        .city("Kharkiv")
+                        .street("Sumska")
+                        .suite("40")
+                        .geo(new Geo.Builder()
+                                .lat(37.3159)
+                                .lng(81.1496)
+                                .build())
+                        .build())
+                .build();
+        return newUser;
     }
 }
